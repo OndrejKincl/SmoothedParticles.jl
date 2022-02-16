d99826e1478019527cf041bee053ff6ef5efdcbd
OndrejKincl <80208777+OndrejKincl@users.noreply.github.com>
2022-02-16 15:49:40 +0100

    importing SmoothedParticles as a package

--- examples/cylinder.jl
+++ examples/cylinder.jl
@@ -17,8 +17,7 @@ All parameters of this benchmark can be found
 module cylinder
 
 using Printf
-include("../src/SmoothedParticles.jl")
-using .SmoothedParticles
+using SmoothedParticles
 
 const folder_name = "results/cylinder"
 
